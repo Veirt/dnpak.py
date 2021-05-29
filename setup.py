76bd8ed06a704d0347229115f70ba6d3caa15b9e
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-29 21:38:19 +0800

    chore(build): publish to pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+VERSION = "1.0.0"
+DESCRIPTION = "A python package to manipulate DragonNest pak file"
+
+setup(
+      name="dnpak-py",
+      version=VERSION,
+      author="Muhammad Dony Mulya",
+      author_email="exlog@protonmail.com",
+      url="https://github.com/ExLog/dnpak-py",
+      description=DESCRIPTION,
+      license="WTFPL",
+      keywords=['dragonnest'],
+)
