ddd5c2f9d6fbbd8b063f095d3eec1e047b50c674
Muhammad Dony Mulya <55097092+ExLog@users.noreply.github.com>
2021-09-30 22:17:54 +0800

    build(setup): change author email and url

--- setup.py
+++ setup.py
@@ -7,24 +7,24 @@ VERSION = "1.0.7"
 DESCRIPTION = "A python package to manipulate DragonNest pak file"
 
 setup(
-      name="dnpak.py",
-      version=VERSION,
-      author="Muhammad Dony Mulya",
-      author_email="exlog@protonmail.com",
-      url="https://github.com/ExLog/dnpak-py",
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      license="WTFPL",
-      keywords=["dragonnest"],
-      packages=["dnpak"],
-      package_dir={"": "src"},
-      classifiers=[
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Intended Audience :: Developers",
-            "License :: Other/Proprietary License"
-      ]
+    name="dnpak.py",
+    version=VERSION,
+    author="Muhammad Dony Mulya",
+    author_email="vveirt@gmail.com",
+    url="https://github.com/Veirt/dnpak.py",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="WTFPL",
+    keywords=["dragonnest"],
+    packages=["dnpak"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+    ],
 )
